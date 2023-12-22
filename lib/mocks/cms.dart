8a52ca2bf1c8c9a171e533816ad9a7861ead9ac6
Ralph Sébastien Placide <placideralph@gmail.com>
2023-12-21 23:26:02 -0500

    feat: add new utils classes

---
+++ lib/mocks/cms.dart
@@ -0,0 +1,6 @@
+import 'package:cook_snap/mocks/pages/start_page_cms.dart';
+
+class CMS {
+  // Add your CMS implementation here
+  final StartPageCMS startPage = StartPageCMS();
+}
---
+++ lib/types/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class TextThemes {
+  static const TextStyle ctaTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.7,
+  );
+}
