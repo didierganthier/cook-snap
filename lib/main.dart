ce59c955b96ae35af9905985d275b1c2d2a01744
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-12-28 14:31:34 -0500

    Add basic folders and files for MVVM

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:cook_snap/app/constants/app_constants.dart';
-import 'package:cook_snap/page_wrapper.dart';
-import 'package:cook_snap/pages/start_page.dart';
+import 'package:cook_snap/app/ui/screens/start/start_page.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
@@ -22,32 +21,7 @@ class MyApp extends StatelessWidget {
         barBackgroundColor: AppColors.primaryColor,
         scaffoldBackgroundColor: AppColors.backgroundColor,
       ),
-      home: PageWrapper(),
+      home: StartPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const StartPage();
   }
 }
