2c09243c4a7049700b9ce39259466f904434b536
Ralph Sébastien Placide <placideralph@gmail.com>
2024-01-13 16:04:32 -0500

    feat: added supabase auth service

--- lib/app/data/services/supabase_service.dart
+++ lib/app/data/services/supabase_service.dart
@@ -1,61 +1,36 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cook_snap/app/data/services/google_auth_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class GoogleSignInService {
-  // googleSignInInstance
-  Future<GoogleSignIn> _googleSignIn() async {
-    try {
-      final webClientId = dotenv.env['WEB_CLIENT_ID']!;
-      final iosClientId = dotenv.env['IOS_CLIENT_ID']!;
+class SupabaseService {
+  final GoogleAuthService _googleAuthService = GoogleAuthService();
+  final supabaseAuthInstance = Supabase.instance.client.auth;
 
-      final GoogleSignIn googleSignIn = GoogleSignIn(
-        scopes: [
-          'email',
-          'https://www.googleapis.com/auth/contacts.readonly',
-        ],
-        clientId: iosClientId,
-        serverClientId: webClientId,
-      );
-
-      return googleSignIn;
-    } catch (e) {
-      throw Exception(e);
-    }
+  SupabaseService() {
+    supabaseAuthInstance.onAuthStateChange.listen((state) {
+      // Handle auth state changes
+    });
   }
 
-  Future<AuthResponse?> signIn() async {
-    try {
-      final googleSignInInstance = await _googleSignIn();
-      final googleUser = await googleSignInInstance.signIn();
-      final googleAuth = await googleUser!.authentication;
-      final accessToken = googleAuth.accessToken;
-      final idToken = googleAuth.idToken;
-
-      if (accessToken == null) {
-        throw 'No Access Token found.';
-      }
-      if (idToken == null) {
-        throw 'No ID Token found.';
-      }
-      final supabaseInstance = Supabase.instance.client;
-      return supabaseInstance.auth.signInWithIdToken(
-        provider: OAuthProvider.google,
-        idToken: idToken,
-        accessToken: accessToken,
-      );
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
-      return null;
-    }
+  Future<void> signInWithGoogle() async {
+    await _googleAuthService.signIn();
+    // Perform additional authentication with Supabase
   }
 
   Future<void> signOut() async {
-    final googleSignInInstance = await _googleSignIn();
-    await googleSignInInstance.signOut();
-    return;
+    await _googleAuthService.signOut();
+    // Perform additional sign out with Supabase
   }
+
+  // Future<UserInfo> getUserInfo() async {
+  // Retrieve user information from Supabase
+  // and return it as UserInfo object
+  // }
+}
+
+class UserInfo {
+  final String name;
+  final String email;
+  final String photoUrl;
+
+  UserInfo({required this.name, required this.email, required this.photoUrl});
 }
